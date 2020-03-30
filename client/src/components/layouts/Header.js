5cfdf1dd7f90a78b3adbbe47f278b7d0a0ad050f
stfurkan <35101659+stfurkan@users.noreply.github.com>
2020-03-30 19:20:34 -0400

    Header style updated

--- client/src/components/layouts/Header.js
+++ client/src/components/layouts/Header.js
@@ -6,28 +6,28 @@ import logo from '../../logo.png';
 function Header({ lang }) {
   return (
     <div className="ui container ">
-      <br />
+      <div className="ui divider"></div>
       <div className="ui stackable secondary menu">
         <div className="item">
           <Link to="/">
-            <div className="ui blue ribbon large label">
+            <div className="ui blue ribbon big label">
               <img src={logo} alt="logo" /> {lang.title}
             </div>
           </Link>
         </div>
-        <Link to="/" className="ui primary basic button item">
+        <Link to="/" className="ui primary basic big button item">
           {lang.home}
         </Link>
-        <Link to="/compare" className="ui primary basic button item">
+        <Link to="/compare" className="ui primary basic big button item">
           {lang.compare}
         </Link>
         <div className="right menu">
-          <Link to="/about" className="ui primary basic button item">
+          <Link to="/about" className="ui primary basic big button item">
             {lang.about}
           </Link>
         </div>
       </div>
-      <br />
+      <div className="ui divider"></div>
     </div>
   );
 }
